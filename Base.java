e648da9a6ae29fd03c9a6426e8fbdcb8648a2d0e
Dzmitry Bohnat <Dzmitry_Bohnat@epam.com>
2020-01-24 18:06:50 +0300

    new commit

--- Base.java
+++ Base.java
@@ -1,84 +1,69 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class Base {
 
-  public static void printString(String text) {
+  public static void printString(String text) {   // General method for printing messages
     System.out.println(text);
   }
 
   public static void main(String[] ags) {
 
-    char operation;
-    double secondNumber, result = 0;
-    Scanner inputChar = new Scanner(System.in);
-    boolean continueInput = false;
+    char operation; // store operator for calculation
+    double secondNumber, firstNumber = 0, result = 0; // store entered numbers and result
+    Scanner inputChar = new Scanner(System.in); // for scanning entered operators
 
-    printString("Welcome to Calculator Application \n Please use + - * / as operator \n Please use N or n after calculation is done to quit");
+    printString("Welcome to Calculator Application \n Please use + - * / as operator \n");
 
     do {
 
-      try{				
-        printString("Enter number");
-        Scanner inputNumber = new Scanner(System.in);
-        result = inputNumber.nextDouble();
-        continueInput = true;
-        }
+      printString("\nEnter first number:");
+      Scanner inputNumber = new Scanner(System.in);
+      firstNumber = inputNumber.nextDouble();
 
-    catch (InputMismatchException e){
+      printString("Enter operator:");
 
-  } while (!continueInput);
+      operation = inputChar.next().charAt(0);
 
-      printString("Enter operator");
+      printString("Enter second number:");
+      Scanner inputSecondNumber = new Scanner(System.in);
+      secondNumber = inputSecondNumber.nextDouble();
+
+      switch (operation) {
+
+      case '+':
+        result = firstNumber + secondNumber;
+        break;
+
+      case '-':
+        result = firstNumber - secondNumber;
+        break;
+
+      case '*':
+        result = firstNumber * secondNumber;
+        break;
+
+      case '/':
+        result = firstNumber / secondNumber;
+        break;
+
+      default:
+        printString("Unknown operation");
+      }
+
+      printString("\nResult:\n"+firstNumber +" "+ operation + " " + secondNumber +" = "+result+"\n");
+
+      printString("Do you want to make new calculation? Please enter N to quit:");
       operation = inputChar.next().charAt(0);
 
       if (operation == 'n' || operation == 'N') {
 
-        printString("BUY! App is closed!");
-        System.exit(0);
 
-      } else {
-
-        printString("Enter number");
-        Scanner inputSecondNumber = new Scanner(System.in);
-        secondNumber = inputSecondNumber.nextDouble();
-
-        switch (operation) {
-
-        case '+':
-          result += secondNumber;
-          break;
-
-        case '-':
-          result -= secondNumber;
-          break;
-
-        case '*':
-          result *= secondNumber;
-          break;
-
-        case '/':
-          result /= secondNumber;
-          break;
-
-        default:
-          printString("Unknown operation");
-        }
-      }
-
-      printString("Your result is: "+result);
-
-      printString("If you want make new calculation enter any char otherwise prin N");
-      operation = inputChar.next().charAt(0);
-
-      if (operation == 'n' || operation == 'N') {
-
-        printString("\n\nThank You, BUY! \n\n\n");
+        printString("\n---------------\nThank You, BUY! \n---------------\n\n");
         System.exit(0);
       }
+
 
     } while (operation != 'N' || operation != 'n');
-  
     System.exit(0);
 
   }
